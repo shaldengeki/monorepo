8764cece77f3415f8892214cbb9623e2e9a5945e
Charles Guo <shaldengeki@gmail.com>
2019-10-24 15:11:00 -0700

    Enable migrations support for pg

--- home-api/api/app.py
+++ home-api/api/app.py
@@ -1,9 +1,22 @@
+import os
+
 from flask import Flask
 from flask_graphql import GraphQLView
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from .schema import schema
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://{user}:{password}@{host}/{db}'.format(
+    user=os.getenv('DB_USER', 'admin'),
+    password=os.getenv('DB_PASS', 'development'),
+    host=os.getenv('DB_HOST', 'pg'),
+    db=os.getenv('DB_NAME', 'api_development')
+)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.add_url_rule(
     '/graphql',
