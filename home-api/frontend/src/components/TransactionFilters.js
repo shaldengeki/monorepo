7e7bf811788dd395b4c6bd6e541326916863e120
Charles Guo <shaldengeki@gmail.com>
2020-08-09 15:05:09 -0700

    Add min, max amount filters

--- home-api/frontend/src/components/TransactionFilters.js
+++ home-api/frontend/src/components/TransactionFilters.js
@@ -21,12 +21,36 @@ const TransactionFilters = (props) => {
     } = props;
 
     return (
-        <DatePicker
-            start={start}
-            end={end}
-            onChangeStart={onChangeStart}
-            onChangeEnd={onChangeEnd}
-        />
+        <div>
+            <DatePicker
+                start={start}
+                end={end}
+                onChangeStart={onChangeStart}
+                onChangeEnd={onChangeEnd}
+            />
+            <div class="mb-6">
+                <label class="block mb-2" for="minAmount">Minimum Amount</label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="minAmount"
+                    type="number"
+                    placeholder="$0.00"
+                    value={minAmount}
+                    onChange={(e) => {onChangeMinAmount(e.target.value)}}
+                />
+            </div>
+            <div class="mb-6">
+                <label class="block mb-2" for="maxAmount">Maximum Amount</label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="maxAmount"
+                    type="number"
+                    placeholder="$0.00"
+                    value={maxAmount}
+                    onChange={(e) => {onChangeMaxAmount(e.target.value)}}
+                />
+            </div>
+        </div>
     )
 }
 export default TransactionFilters;
