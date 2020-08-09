a7ab7f1848a0238590f884b10cbaea91dda17d38
Charles Guo <shaldengeki@gmail.com>
2020-08-08 20:01:57 -0700

    Add DatePicker component to control chart, list data

--- home-api/frontend/src/components/TransactionDisplay.js
+++ home-api/frontend/src/components/TransactionDisplay.js
@@ -7,10 +7,22 @@ const TransactionDisplay = () => {
     const latestDate = Math.round(Date.now() / 1000 | 0);
     const earliestDate = latestDate - (365 * 24 * 60 * 60);
 
+    const [start, setStart] = useState(earliestDate);
+    const [end, setEnd] = useState(latestDate);
+
+    const parsedStart = Date.parse(start);
+    const parsedEnd = Date.parse(end);
+
+    const validDates = !(_.isNaN(start) || _.isNaN(end));
+
+    const chartElement = validDates ? (<TransactionChart earliestDate={parsedStart} latestDate={parsedEnd} />) : (<div />);
+    const listElement = validDates ? (<TransactionList earliestDate={parsedStart} latestDate={parsedEnd} />) : (<div />);
+
     return (
         <div>
-            <TransactionChart earliestDate={earliestDate} latestDate={latestDate} />
-            <TransactionList earliestDate={earliestDate} latestDate={latestDate} />
+            <DatePicker start={start} end={end} onChangeStart={setStart} onChangeEnd={setEnd} />
+            {chartElement}
+            {listElement}
         </div>
     );
 }
