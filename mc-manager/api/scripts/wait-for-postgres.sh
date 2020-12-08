184f1794a163392db19537f3ee751b670ea07404
Charles Guo <shaldengeki@gmail.com>
2020-12-08 15:08:29 -0800

    provide db name

--- mc-manager/api/scripts/wait-for-postgres.sh
+++ mc-manager/api/scripts/wait-for-postgres.sh
@@ -6,7 +6,7 @@ set -e
 shift
 cmd="$@"
 
-until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USERNAME" -c '\q'; do
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USERNAME" -d "$DATABASE_NAME" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
