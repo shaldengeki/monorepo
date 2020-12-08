b9290c50e8ec1a97979410545bbb9478f69562ce
Charles Guo <shaldengeki@gmail.com>
2020-12-08 12:35:29 -0800

    Add foreign key, index on server_id

--- mc-manager/api/migrations/versions/4d81354c3a36_create_server_logs_table.py
+++ mc-manager/api/migrations/versions/4d81354c3a36_create_server_logs_table.py
@@ -7,6 +7,7 @@ Create Date: 2020-12-08 12:09:39.612244
 """
 from alembic import op
 import sqlalchemy as sa
+import datetime
 
 
 # revision identifiers, used by Alembic.
@@ -20,9 +21,26 @@ def upgrade():
     op.create_table(
         "server_logs",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("created", sa.DateTime, nullable=False),
+        sa.Column("server_id", sa.Integer, nullable=False),
+        sa.Column(
+            "created", sa.DateTime, nullable=False, default=datetime.datetime.utcnow
+        ),
         sa.Column("state", sa.Unicode(100), nullable=False),
         sa.Column("error", sa.Unicode(500)),
+    )
+    op.create_foreign_key(
+        "server_logs_servers_server_id",
+        "server_logs",
+        "servers",
+        ["server_id"],
+        ["id"],
+        "CASCADE",
+        "CASCADE",
+    )
+    op.create_index(
+        "server_logs_server_id",
+        "server_logs",
+        ["server_id", "created"],
     )
 
 
