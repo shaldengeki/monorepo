96d9f012e8b1d3e4ce497d95db4224bf79505f81
Charles Guo <shaldengeki@gmail.com>
2020-12-07 23:24:05 -0800

    Initial API skeleton for server listing

---
+++ mc-manager/api/models/server.py
@@ -0,0 +1,17 @@
+from ..app import db
+
+
+class Server(db.Model):
+    __tablename__ = "servers"
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+    created_by = db.Column(db.Unicode(100), nullable=False)
+    name = db.Column(db.Unicode(100), nullable=False)
+    port = db.Column(db.Integer, nullable=False)
+    timezone = db.Column(db.Unicode(100), nullable=False)
+    zipfile = db.Column(db.Unicode(100), nullable=False)
+    motd = db.Column(db.Unicode(100))
+    memory = db.Column(db.Unicode(3), nullable=False)
+
+    def __repr__(self):
+        return "<Server {id}>".format(id=self.id)
