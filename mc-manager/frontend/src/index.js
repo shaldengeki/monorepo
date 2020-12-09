a977bdf69c5dec18b7de709dca4aa15b95443732
Charles Guo <shaldengeki@gmail.com>
2020-12-08 18:00:08 -0800

    Don't override env vars in compose; default API host/port

--- mc-manager/frontend/src/index.js
+++ mc-manager/frontend/src/index.js
@@ -11,8 +11,11 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
 const cache = new InMemoryCache();
+
+const { REACT_APP_API_HOST = 'localhost', REACT_APP_API_PORT = '5000' } = process.env;
+
 const link = new HttpLink({
-  uri: `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/graphql`
+  uri: `http://${REACT_APP_API_HOST}:${REACT_APP_API_PORT}/graphql`
 });
 
 const client = new ApolloClient({
