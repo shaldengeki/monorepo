1950ce64534e6f3e03d07d2e7e3146fdc019b527
Charles Guo <shaldengeki@gmail.com>
2020-12-08 21:31:32 -0800

    Update packages

--- mc-manager/frontend/src/index.js
+++ mc-manager/frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './App.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient } from 'apollo-client';
@@ -24,11 +23,15 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
+  <React.StrictMode>
     <ApolloProvider client={client} >
-        <App />
-    </ApolloProvider>, document.getElementById('root'));
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
