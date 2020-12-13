b82e68bc5433d1800d8e5f21435d745e6ddeb7a4
Charles Guo <shaldengeki@gmail.com>
2020-12-13 10:59:34 -0800

    remove alt-text

--- mc-manager/frontend/src/components/Header.tsx
+++ mc-manager/frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="pb-3">
         <span className="w-24 h-24 inline-block text-center">
-          <Logo className="inline max-h-24 max-w-24" alt="Logo" />
+          <Logo className="inline max-h-24 max-w-24" />
         </span>
         <span className="text-5xl text-gray-50">
             Minecraft Server Manager
