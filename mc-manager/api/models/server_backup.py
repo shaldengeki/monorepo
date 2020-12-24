8c5ff7da7a5ae4f8409d8ede3ba66fc75b70ba96
Charles Guo <shaldengeki@gmail.com>
2020-12-24 15:21:11 -0800

    Add server backups model

---
+++ mc-manager/api/models/server_backup.py
@@ -0,0 +1,20 @@
+import datetime
+
+from ..app import db
+from .server import Server
+
+
+class ServerBackup(db.Model):
+    __tablename__ = "server_backups"
+    id = db.Column(db.Integer, primary_key=True)
+    server_id = db.Column(db.Integer, db.ForeignKey(Server.id), nullable=False)
+    created = db.Column(
+        db.TIMESTAMP(timezone=True), default=datetime.datetime.utcnow, nullable=False
+    )
+    state = db.Column(db.Unicode(100), nullable=False)
+    error = db.Column(db.Unicode(500), nullable=True)
+    remote_path = db.Column(db.Unicode(500), nullable=False)
+    server = db.relationship("Server", back_populates="backups")
+
+    def __repr__(self):
+        return "<ServerBackup {id}>".format(id=self.id)
