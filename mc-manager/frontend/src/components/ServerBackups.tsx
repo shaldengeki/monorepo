b448715bc94ba67e4eaad6edcb7406588ec68780
Charles Guo <shaldengeki@gmail.com>
2020-12-27 21:51:06 -0800

    Support enqueueing of backup restoration

--- mc-manager/frontend/src/components/ServerBackups.tsx
+++ mc-manager/frontend/src/components/ServerBackups.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import _ from 'lodash'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 
 import { serverBackupStatusSymbol } from '../Utils'
@@ -9,6 +9,7 @@ import Table from './Table'
 const GET_SERVER_BACKUPS = gql`
     query ServerBackups($name: String) {
         servers(name: $name) {
+            id
             backups {
                 id
                 created
@@ -19,10 +20,26 @@ const GET_SERVER_BACKUPS = gql`
     }
 `
 
+const ENQUEUE_SERVER_BACKUP = gql`
+    mutation EnqueueServerBackup($serverId:Int!, $backupId:Int!) {
+        createServerLog(
+            serverId:$serverId,
+            state:restore_queued,
+            backupId:$backupId
+        ) {
+            state
+            backup {
+                id
+            }
+        }
+    }
+`
+
 type ServerBackup = {
     created: string,
     state: string,
     error?: string,
+    restoreLink: any
   }
 
 type ServerBackupsProps = {
@@ -35,6 +52,8 @@ const ServerBackups = ({ name }: ServerBackupsProps) => {
     pollInterval: 60_000
   })
 
+  const [enqueueServerBackup, { data: enqueueData, loading: enqueueLoading, error: enqueueError }] = useMutation(ENQUEUE_SERVER_BACKUP)
+
   const loadingDisplay = <h1>Loading backups...</h1>
   const errorDisplay = <h1>Error loading backups!</h1>
 
@@ -45,11 +64,23 @@ const ServerBackups = ({ name }: ServerBackupsProps) => {
 
   const formattedBackups : Array<ServerBackup> = _.map(server.backups || [], (backup) => {
     const createdFormatted = new Date(backup.created * 1000).toLocaleDateString('en-US')
+    const restoreLink = (serverId: number, backupId: number) => {
+      if (enqueueLoading) return '🕜Enqueueing restoration...'
+      if (enqueueError) return '❌Enqueueing backup failed'
+      if (enqueueData && enqueueData.backup && enqueueData.backup.id) return '✅Restoration enqueued!'
+      return (
+            <a onClick={e => {
+              e.preventDefault()
+              enqueueServerBackup({ variables: { serverId, backupId } })
+            }}>Restore</a>
+      )
+    }
 
     return {
       created: createdFormatted,
       state: `${serverBackupStatusSymbol(backup.state)} ${backup.state}`,
-      error: backup.error
+      error: backup.error,
+      restoreLink: restoreLink(server.id, backup.id)
     }
   })
 
