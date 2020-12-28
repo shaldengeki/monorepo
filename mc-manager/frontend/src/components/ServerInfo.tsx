f5228c2daf6f882eaa86fd31c058676e0fc07b95
Charles Guo <shaldengeki@gmail.com>
2020-12-27 23:18:19 -0800

    Also add header to info box

--- mc-manager/frontend/src/components/ServerInfo.tsx
+++ mc-manager/frontend/src/components/ServerInfo.tsx
@@ -80,38 +80,41 @@ const ServerInfo = ({ name }: ServerInfoProps) => {
   const server = data.servers[0]
 
   return (
-    <table className="table-auto border-collapse border">
-      <tbody>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">Mod</td>
-          <td className="border border-gray-300 px-4 py-2">{server.zipfile}</td>
-        </tr>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">Latest status</td>
-          <td className="border border-gray-300 px-4 py-2">{displayLog(server.latestLog)}</td>
-        </tr>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">URL</td>
-          <td className="border border-gray-300 px-4 py-2">{displayServerUrl(server.port)}</td>
-        </tr>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">Created</td>
-          <td className="border border-gray-300 px-4 py-2">{timeAgo(server.created)}</td>
-        </tr>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">Creator</td>
-          <td className="border border-gray-300 px-4 py-2">{server.createdBy}</td>
-        </tr>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">MOTD</td>
-          <td className="border border-gray-300 px-4 py-2">{server.motd}</td>
-        </tr>
-        <tr>
-          <td className="font-medium border border-gray-300 px-4 py-2">Latest backup</td>
-          <td className="border border-gray-300 px-4 py-2">{displayBackup(server.latestBackup)}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <p className="text-3xl">Info</p>
+      <table className="table-auto border-collapse border">
+        <tbody>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">Mod</td>
+            <td className="border border-gray-300 px-4 py-2">{server.zipfile}</td>
+          </tr>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">Latest status</td>
+            <td className="border border-gray-300 px-4 py-2">{displayLog(server.latestLog)}</td>
+          </tr>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">URL</td>
+            <td className="border border-gray-300 px-4 py-2">{displayServerUrl(server.port)}</td>
+          </tr>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">Created</td>
+            <td className="border border-gray-300 px-4 py-2">{timeAgo(server.created)}</td>
+          </tr>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">Creator</td>
+            <td className="border border-gray-300 px-4 py-2">{server.createdBy}</td>
+          </tr>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">MOTD</td>
+            <td className="border border-gray-300 px-4 py-2">{server.motd}</td>
+          </tr>
+          <tr>
+            <td className="font-medium border border-gray-300 px-4 py-2">Latest backup</td>
+            <td className="border border-gray-300 px-4 py-2">{displayBackup(server.latestBackup)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
