f947cdec1dea44f992aa98eb20d6ee92cfa914f5
Charles Guo <shaldengeki@gmail.com>
2020-12-27 18:43:39 -0800

    Add utils

---
+++ mc-manager/frontend/src/Utils.tsx
@@ -0,0 +1,23 @@
+export const timeAgo = (epochTime: number): string => {
+  const happened = new Date(epochTime * 1000)
+  const now = new Date()
+  const diff = (now.getTime() - happened.getTime()) / 1000
+
+  if (diff < 10) {
+    return 'just now'
+  } else if (diff < 60) {
+    return Math.round(diff) + ' seconds ago'
+  } else if (diff < (60 * 60)) {
+    return Math.round(diff / 60) + ' minutes ago'
+  } else if (diff < (60 * 60 * 24)) {
+    return Math.round(diff / (60 * 60)) + ' hours ago'
+  } else if (diff < (60 * 60 * 24 * 7)) {
+    return Math.round(diff / (60 * 60 * 24)) + ' days ago'
+  } else if (diff < (60 * 60 * 24 * 30)) {
+    return Math.round(diff / (60 * 60 * 24 * 7)) + ' weeks ago'
+  } else if (diff < (60 * 60 * 24 * 365)) {
+    return Math.round(diff / (60 * 60 * 24 * 30)) + ' months ago'
+  } else {
+    return Math.round(diff / (60 * 60 * 24 * 365)) + ' years ago'
+  }
+}
