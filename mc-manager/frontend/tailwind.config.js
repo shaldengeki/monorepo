b1e34ecae48180e19d7c80a627e3e1233dba9716
Charles OuGuo <shaldengeki@gmail.com>
2021-07-22 04:36:37 +0000

    remove unnecessary commas, semicolons

--- mc-manager/frontend/tailwind.config.js
+++ mc-manager/frontend/tailwind.config.js
@@ -1,14 +1,14 @@
-export const prefix = '';
+export const prefix = ''
 export const purge = {
   // enabled: process.env.NODE_ENV === 'production',
   enabled: true,
   content: ['./src/**/*.{html,ts}']
-};
-export const darkMode = 'class';
+}
+export const darkMode = 'class'
 export const theme = {
-  extend: {},
-};
+  extend: {}
+}
 export const variants = {
-  extend: {},
-};
-export const plugins = [];
+  extend: {}
+}
+export const plugins = []
