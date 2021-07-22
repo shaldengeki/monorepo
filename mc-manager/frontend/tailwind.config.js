a8db663c1e34ba6f3adfbaa7c276e3acd9eb8cce
Charles OuGuo <shaldengeki@gmail.com>
2021-07-22 08:12:58 +0000

    Turn off purging of unused styles

--- mc-manager/frontend/tailwind.config.js
+++ mc-manager/frontend/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   prefix: '',
   purge: {
     // enabled: process.env.NODE_ENV === 'production',
-    enabled: true,
+    enabled: false,
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   },
   darkMode: 'class', // or 'media' or 'class'
