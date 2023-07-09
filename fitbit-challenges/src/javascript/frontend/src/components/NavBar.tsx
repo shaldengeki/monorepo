77c1f64f309d1fd0482980b23989e0579b377b76
Charles OuGuo <shaldengeki@gmail.com>
2023-07-09 01:09:23 -0400

    Add Challenges link to topbar (#222)
    - Add Challenges link to topbar
    - Add styling to login text, as well

--- fitbit-challenges/src/javascript/frontend/src/components/NavBar.tsx
+++ fitbit-challenges/src/javascript/frontend/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import fitbit from './fitbit.png';
+import logo192 from './logo192.png';
 import { Link } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
 
@@ -24,24 +25,32 @@ const NavBar = (props: NavBarProps) => {
     );
 
     return (
-        <div className="leading-6 w-full sticky border-b border-b-slate-50 py-4 px-1 bg-blue-200 dark:bg-slate-900 dark:text-slate-400">
-            <div className="relative flex items-center">
-                {
-                    loading && <p>Loading...</p>
-                }
-                {
-                    error && <p>Error loading login state</p>
-                }
-                {
-                    data && data.currentUser === null &&
-                    <Link to={'/auth'} className="ml-auto">
-                        <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
-                        <span>Sign in with Fitbit</span>
-                    </Link>
-                }
-                {
-                    data && data.currentUser && <p className="ml-auto">{data.currentUser.displayName}</p>
-                }
+        <div className="sticky top-0 z-40 w-full backdrop-blur flex-none lg:border-b lg:border-slate-900/10 lg:dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+            <div className="max-w-screen-2xl mx-auto">
+                <div className="py-4 mx-4 lg:mx-0 lg:px-8 relative flex items-center gap-2 dark:text-slate-400">
+                    <img className="flex-none h-6 inline" src={logo192} alt="Fitbit app icon" />
+                    <div className="dark:hover:text-slate-300 hover:text-slate-500">
+                        <Link to={'/challenges'}>
+                            <p className="font-bold">Challenges</p>
+                        </Link>
+                    </div>
+                    {
+                        loading && <p>Loading...</p>
+                    }
+                    {
+                        error && <p>Error loading login state</p>
+                    }
+                    {
+                        data && data.currentUser === null &&
+                        <Link to={'/auth'} className="ml-auto dark:hover:text-slate-300 hover:text-slate-500">
+                            <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
+                            <span className="font-bold">Sign in with Fitbit</span>
+                        </Link>
+                    }
+                    {
+                        data && data.currentUser && <p className="ml-auto">{data.currentUser.displayName}</p>
+                    }
+                </div>
             </div>
         </div>
     );
