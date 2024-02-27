40b3b1e5f70ad0a1d4774568cc7e2ebe5162abba
Charles OuGuo <shaldengeki@gmail.com>
2024-02-27 15:15:26 -0500

    Add wait-for script to API container (#21)
    - **Add wait-for script in API container**
    - **Update lockfiles**

--- src/python/fitbit_challenges/api/__main__.py
+++ src/python/fitbit_challenges/api/__main__.py
@@ -1,4 +1,6 @@
 from src.python.fitbit_challenges.api.app import app
+from src.python.fitbit_challenges.scripts.wait_for_postgres import wait_for_postgres
 
 if __name__ == "__main__":
+    wait_for_postgres()
     app.run()
---
+++ src/python/fitbit_challenges/scripts/wait_for_postgres.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+import time
+
+import pg8000.native
+
+
+def wait_for_postgres() -> None:
+    while True:
+        try:
+            pg8000.native.Connection(
+                os.getenv("DB_USERNAME", "admin"),
+                host=os.getenv("DB_HOST", "pg"),
+                port=os.getenv("DB_PORT", 5432),
+                database=os.getenv("DATABASE_NAME", "api_development"),
+                password=os.getenv("DB_PASSWORD", "development"),
+            )
+        except pg8000.exceptions.InterfaceError:
+            print("Postgres is unavailable - sleeping")
+            time.sleep(1)
+            continue
+        else:
+            break
+
+    print("Postgres is up - executing command")
+
+
+if __name__ == "__main__":
+    wait_for_postgres()
+    subprocess.run(sys.argv[1:])
