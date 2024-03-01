d668cfcb49b2288919e62b98b043223f0a36c6cc
Charles OuGuo <shaldengeki@gmail.com>
2024-02-29 21:59:41 -0500

    Set up migrations container to run migrations (#22)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/python/fitbit_challenges/api/__main__.py
+++ src/python/fitbit_challenges/api/__main__.py
@@ -1,6 +1,4 @@
 from src.python.fitbit_challenges.api.app import app
-from src.python.fitbit_challenges.scripts.wait_for_postgres import wait_for_postgres
 
 if __name__ == "__main__":
-    wait_for_postgres()
     app.run()
