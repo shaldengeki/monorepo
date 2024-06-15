36628a349f111f3dd9fa11b9327ce8e6b127195f
Charles OuGuo <shaldengeki@gmail.com>
2024-06-15 15:24:10 -0500

    Fix all the fitbit-challenges paths that got broken (#149)

--- fitbit_challenges/api/migrations/__main__.py
+++ fitbit_challenges/api/migrations/__main__.py
@@ -8,12 +8,8 @@ from python.runfiles import Runfiles
 if __name__ == "__main__":
     # Copy the alembic.ini.
     r = Runfiles.Create()
-    alembic_ini_src = r.Rlocation(
-        "_main/src/python/fitbit_challenges/api/migrations/alembic.ini"
-    )
-    shutil.copyfile(
-        alembic_ini_src, "/src/python/fitbit_challenges/api/migrations/alembic.ini"
-    )
+    alembic_ini_src = r.Rlocation("_main/fitbit_challenges/api/migrations/alembic.ini")
+    shutil.copyfile(alembic_ini_src, "/fitbit_challenges/api/migrations/alembic.ini")
 
     with app.app_context():
-        upgrade(directory="/src/python/fitbit_challenges/api/migrations")
+        upgrade(directory="/fitbit_challenges/api/migrations")
