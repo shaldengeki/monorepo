ef170b526175ac2a990d2df4b698e6a771c51e1b
Charles Guo <shaldengeki@gmail.com>
2024-06-22 15:40:05 -0400

    Include frontend assets during build

--- fitbit_challenges/frontend/webpack.config.js
+++ fitbit_challenges/frontend/webpack.config.js
@@ -1,20 +1,52 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   module: {
     rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(ico|json)$/,
+        exclude: /node_modules/,
+        use: [{loader: "file-loader"}]
+      },
+    ],
+  },
+  output: {
+    publicPath: '',
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'public/manifest.json', to: ''},
+        {from: 'public/robots.txt', to: ''},
       ],
-    },
-   ],
- },
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      favicon: 'public/favicon.ico',
+      inject: true,
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  resolve: {
+    alias: {
+      process: "process/browser",
+    }
+  }
 };
