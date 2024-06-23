cd3199ec438997556bb22be5fa0f73957c31921a
Charles Guo <shaldengeki@gmail.com>
2024-06-23 18:42:56 -0400

    Don't checkout main initially, and default remote branch to main

--- scripts/merge-repo.sh
+++ scripts/merge-repo.sh
@@ -2,6 +2,8 @@
 set -euxo pipefail
 
 REPO=$1
+BRANCH="${2:-main}"
+
 cd ~
 sudo rm -rf $REPO
 git clone https://github.com/shaldengeki/$REPO
@@ -13,9 +15,8 @@ git clone https://github.com/shaldengeki/$REPO
 
 (
     cd ~/monorepo/
-    git switch main
     git remote add $REPO ../$REPO
     git fetch $REPO --no-tags
-    EDITOR=true git merge --allow-unrelated-histories $REPO/main
+    EDITOR=true git merge --allow-unrelated-histories $REPO/$BRANCH
     git remote remove $REPO
 )
