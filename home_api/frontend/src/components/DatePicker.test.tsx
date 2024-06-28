da0e6a2f89041d85af35fff2f148c15510d5b50c
Charles OuGuo <shaldengeki@gmail.com>
2024-06-28 19:31:40 -0400

    Fix test

--- home_api/frontend/src/components/DatePicker.test.tsx
+++ home_api/frontend/src/components/DatePicker.test.tsx
@@ -4,6 +4,6 @@ import React from 'react';
 
 it('should render', async () => {
     render(
-        <DatePicker start={1} end={2} onChangeStart={() => {}} onChangeEnd={() => {}} />
+        <DatePicker start={"2024-06-01"} end={"2024-06-10"} onChangeStart={() => {}} onChangeEnd={() => {}} />
     );
 });
