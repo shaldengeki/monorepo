61da6d51df319f8c0fd743e8e2972acaa96564aa
Charles Guo <shaldengeki@gmail.com>
2024-07-15 15:18:38 -0400

    Add Ark Nova stats skeleton

---
+++ ark_nova_stats/api/gql/schema.py
@@ -0,0 +1,19 @@
+from graphql import GraphQLObjectType, GraphQLSchema
+
+from ark_nova_stats.api.gql.types.example_model import (
+    example_model_field,
+)
+from ark_nova_stats.models import (
+    ExampleModel
+)
+
+
+def Schema(app):
+    return GraphQLSchema(
+        query=GraphQLObjectType(
+            name="Query",
+            fields={
+                "testModel": example_model_field(ExampleModel),
+            },
+        ),
+    )
---
+++ ark_nova_stats/models_test.py
@@ -0,0 +1,12 @@
+import datetime
+import decimal
+from typing import Generator
+
+from ark_nova_stats.models import (
+    TestModel
+)
+
+
+class TestTestModel:
+    def test_sample(self):
+        assert TestModel(id=1)
