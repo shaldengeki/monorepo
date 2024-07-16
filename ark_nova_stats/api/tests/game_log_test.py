0192a6f03490234b83ffedaf1b959513d89d3799
Charles Guo <shaldengeki@gmail.com>
2024-07-15 23:18:35 -0400

    Set up mutation test for game log submission

---
+++ ark_nova_stats/api/tests/game_log_test.py
@@ -0,0 +1,32 @@
+from flask.testing import FlaskClient
+import pytest
+import sys
+import urllib.parse
+
+from ark_nova_stats.api.tests.fixtures import app, client
+from python.runfiles import Runfiles
+
+def test_submit_game_log(client: FlaskClient) -> None:
+    r = Runfiles.Create()
+    sample_game_fixture = r.Rlocation("_main/ark_nova_stats/api/tests/fixtures/sample_game.log.json")
+    with open(sample_game_fixture, "r") as sample_game_logfile:
+        game_log = sample_game_logfile.read()
+
+    response = client.post(
+        "/graphql",
+        json={
+            "query": """
+                mutation {
+                    submitGameLog(
+                        log: \"""" + urllib.parse.quote(game_log) + """\"
+                    ) {
+                        success
+                    }
+                }
+            """
+        },
+    )
+    assert response.json["data"]["success"]
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__] + sys.argv[1:]))
--- ark_nova_stats/models_test.py
+++ ark_nova_stats/models_test.py
@@ -1,5 +1,7 @@
 import datetime
 import decimal
+import pytest
+import sys
 from typing import Generator
 
 from ark_nova_stats.models import ExampleModel
@@ -8,3 +10,6 @@ from ark_nova_stats.models import ExampleModel
 class TestExampleModel:
     def test_sample(self):
         assert ExampleModel(id=1)
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__] + sys.argv[1:]))
