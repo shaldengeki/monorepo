c7891d9393e94678285c53ba13cd7fe73f42155a
Charles Guo <shaldengeki@gmail.com>
2024-07-21 10:52:20 -0400

    Only include play actions

--- ark_nova_stats/emu_cup/analyze_games.py
+++ ark_nova_stats/emu_cup/analyze_games.py
@@ -22,21 +22,45 @@ def list_game_datafiles() -> Iterator[Path]:
 
 def main() -> int:
     all_cards: Counter[str] = Counter()
+    event_logs: set[str] = set()
+    skipped_event_logs: set[str] = set()
+    play_log_actions = [
+        "plays",
+        "gains",
+        "supports a conservation project",
+        "adds",
+        "releases",
+        "and places it in",
+        "gets an extra action",
+        "moves",
+        "places it using",
+        "buys",
+    ]
     for p in list_game_datafiles():
-        print(p)
+        # print(p)
         with open(p, "r") as f:
             log = GameLog(**json.loads(f.read().strip()))
-        game_cards = set(
-            event_data.args["card_name"]
-            for event in log.data.logs
-            for event_data in event.data
-            if "card_name" in event_data.args
-        )
+
+        game_cards = set()
+        for event in log.data.logs:
+            for event_data in event.data:
+                # If not a card action, skip.
+                if "card_name" not in event_data.args:
+                    continue
+
+                # If not playing or supporting a card, skip.
+                if not any(
+                    play_action in event_data.log for play_action in play_log_actions
+                ):
+                    continue
+
+                game_cards.add(event_data.args["card_name"])
+
         all_cards.update(game_cards)
 
     print("Most common cards:")
-    for card, count in all_cards.most_common(20):
-        print(f"  - {card}: {count}")
+    for card, count in all_cards.most_common(10):
+        print(f"  - {card}")
 
     return 0
 
