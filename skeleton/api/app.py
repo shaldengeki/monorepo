ac80eae81ccd20bb776dab86fa7c5486a8d98fa7
Charles OuGuo <shaldengeki@gmail.com>
2024-07-23 19:13:52 -0400

    Clean up some of the imports (#285)

--- skeleton/api/app.py
+++ skeleton/api/app.py
@@ -1,13 +1,7 @@
-import datetime
-from datetime import timezone
-from typing import Optional
-
-from flask import abort, redirect, request, session
 from graphql_server.flask import GraphQLView  # type: ignore
 
-from skeleton import models
 from skeleton.api.gql import schema
-from skeleton.config import app, db
+from skeleton.config import app
 
 app.add_url_rule(
     "/graphql",
--- skeleton/config.py
+++ skeleton/config.py
@@ -1,5 +1,3 @@
-import os
-
 from base.flask_app import FlaskApp
 
 app, cors, db, migrate = FlaskApp(__name__)
