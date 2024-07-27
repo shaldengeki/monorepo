61a961e0ffe09113fb5300372dd9367f935b5495
Charles OuGuo <shaldengeki@gmail.com>
2024-07-26 21:52:28 -0400

    In extension, send data to API (#292)
    Fixes #248

--- ark_nova_stats/extension/detectivekookaburra.js
+++ ark_nova_stats/extension/detectivekookaburra.js
@@ -1,19 +1,66 @@
-let pattern = "https://developer.mozilla.org/*";
-const targetUrl =
-  "https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension/frog.jpg";
+let pattern = "https://boardgamearena.com/*";
+let apiEndpoint = "https://api.arknova.ouguo.us/graphql";
 
-function redirect(requestDetails) {
-  console.log(`Redirecting: ${requestDetails.url}`);
-  if (requestDetails.url === targetUrl) {
+function handleRequest(requestDetails) {
+  console.log(`Handling: ${requestDetails.url}`);
+  if (!requestDetails.url.includes("logs.html")) {
+    console.log("Not logs.html, skipping");
     return;
   }
-  return {
-    redirectUrl: targetUrl,
-  };
+
+  let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  const data = [];
+
+  filter.ondata = (event) => {
+    data.push(event.data);
+  }
+
+  filter.onstop = (event) => {
+    let str = "";
+    if (data.length === 1) {
+      str = decoder.decode(data[0]);
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        const stream = i !== data.length - 1;
+        str += decoder.decode(data[i], { stream });
+      }
+    }
+    console.log("Received data", str);
+
+    let apiHeaders = new Headers();
+    apiHeaders.append("Content-Type", "application/json");
+
+    let apiRequest = new Request(apiEndpoint, {
+      method: "POST",
+      headers: apiHeaders,
+      body: JSON.stringify({
+        query: "mutation SubmitGameLog($logs: String!) {\n  submitGameLogs(logs: $logs) {\n    id\n  }\n}",
+        variables: {
+          logs: str,
+        },
+        operationName: "SubmitGameLog",
+      })
+    });
+    let apiPromise = fetch(apiRequest).then(
+      data => {
+        console.log(`Made API request: ${data.json()}`)
+      }
+    )
+
+    filter.write(encoder.encode(str));
+    filter.disconnect();
+
+    return apiPromise;
+  }
+
+  return {};
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  redirect,
-  { urls: [pattern], types: ["image"] },
+  handleRequest,
+  { urls: [pattern] },
   ["blocking"],
 );
