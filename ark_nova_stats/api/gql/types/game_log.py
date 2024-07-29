91d3da3065e8fc97999905e0977575db364f778a
Charles Guo <shaldengeki@gmail.com>
2024-07-29 01:16:38 -0400

    Expose game statistics in the API

--- ark_nova_stats/api/gql/types/game_log.py
+++ ark_nova_stats/api/gql/types/game_log.py
@@ -1,5 +1,4 @@
 import json
-import logging
 from typing import Any, Optional, Type
 
 from graphql import (
@@ -11,6 +10,7 @@ from graphql import (
     GraphQLObjectType,
     GraphQLString,
 )
+from sqlalchemy import desc
 
 from ark_nova_stats.bga_log_parser.game_log import GameLog as ParsedGameLog
 from ark_nova_stats.config import app, db
@@ -71,7 +71,7 @@ game_log_filters: dict[str, GraphQLArgument] = {
 
 def game_log_field(game_log: type[GameLogModel]) -> GraphQLField:
     return GraphQLField(
-        game_log_type,
+        GraphQLNonNull(game_log_type),
         args=game_log_filters,
         resolve=lambda root, info, **args: fetch_game_log(game_log, args),
     )
@@ -109,7 +109,7 @@ def submit_game_logs_field(
     game_log_model: Type[GameLogModel],
 ) -> GraphQLField:
     return GraphQLField(
-        game_log_type,
+        GraphQLNonNull(game_log_type),
         description="Submit logs for a game.",
         args={
             "logs": GraphQLArgument(
@@ -121,6 +121,25 @@ def submit_game_logs_field(
     )
 
 
+def fetch_game_logs(game_log_model: Type[GameLogModel]) -> list[GameLogModel]:
+    return game_log_model.query.all()
+
+
+def game_logs_field(
+    game_log_model: Type[GameLogModel],
+) -> GraphQLField:
+    return GraphQLField(
+        GraphQLNonNull(GraphQLList(game_log_type)),
+        description="List all game logs.",
+        args={},
+        resolve=lambda root, info, **args: fetch_game_logs(game_log_model),
+    )
+
+
+def user_bga_id_resolver(user: UserModel, info, **args) -> int:
+    return user.bga_id
+
+
 def user_fields() -> dict[str, GraphQLField]:
     return {
         "id": GraphQLField(
@@ -130,6 +149,7 @@ def user_fields() -> dict[str, GraphQLField]:
         "bgaId": GraphQLField(
             GraphQLNonNull(GraphQLInt),
             description="The BGA id of the user.",
+            resolve=user_bga_id_resolver,
         ),
         "name": GraphQLField(
             GraphQLNonNull(GraphQLString),
@@ -147,3 +167,57 @@ user_type = GraphQLObjectType(
     description="A game log entry.",
     fields=user_fields,
 )
+
+
+def stats_fields() -> dict[str, GraphQLField]:
+    return {
+        "numGameLogs": GraphQLField(
+            GraphQLNonNull(GraphQLInt),
+            description="Number of game logs in the database.",
+        ),
+        "numPlayers": GraphQLField(
+            GraphQLNonNull(GraphQLInt),
+            description="Number of players in the database.",
+        ),
+        "mostRecentSubmission": GraphQLField(
+            GraphQLInt,
+            description="UNIX timestamp for the most recently-submitted game in the database.",
+        ),
+    }
+
+
+stats_type = GraphQLObjectType(
+    "Stats",
+    description="A game log entry.",
+    fields=stats_fields,
+)
+
+
+def fetch_stats(
+    game_log_model: Type[GameLogModel], user_model: Type[UserModel]
+) -> dict:
+    most_recent_submission = game_log_model.query.order_by(
+        desc(game_log_model.created_at)
+    ).first()
+    if most_recent_submission is None:
+        most_recent_time = None
+    else:
+        most_recent_time = int(most_recent_submission.created_at.timestamp())
+
+    return {
+        "numGameLogs": game_log_model.query.count(),
+        "numPlayers": user_model.query.count(),
+        "mostRecentSubmission": most_recent_time,
+    }
+
+
+def stats_field(
+    game_log_model: Type[GameLogModel],
+    user_model: Type[UserModel],
+) -> GraphQLField:
+    return GraphQLField(
+        GraphQLNonNull(stats_type),
+        description="Fetch database statistics.",
+        args={},
+        resolve=lambda root, info, **args: fetch_stats(game_log_model, user_model),
+    )
