95f9b48fa031e4aac0423c5fae41231a86547e73
Charles Guo <shaldengeki@gmail.com>
2024-07-30 18:58:42 -0400

    In skeleton, add worker code

---
+++ skeleton/worker/app.py
@@ -0,0 +1,19 @@
+import time
+
+from fitbit_challenges.config import app, db
+
+max_delay = 10
+
+
+def main() -> int:
+    with app.app_context():
+        while True:
+            start = time.time()
+            # Do work here.
+            delay = (start + max_delay) - time.time()
+            if delay > 0:
+                time.sleep(delay)
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
