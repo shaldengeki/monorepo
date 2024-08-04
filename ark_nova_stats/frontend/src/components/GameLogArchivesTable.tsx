500d74722103959f6f6d1414d0ccadd9c4582516
Charles Guo <shaldengeki@gmail.com>
2024-08-03 20:44:20 -0400

    Format table a little

--- ark_nova_stats/frontend/src/components/GameLogArchivesTable.tsx
+++ ark_nova_stats/frontend/src/components/GameLogArchivesTable.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import {getDate} from '../DateUtils';
 import GameLogArchive from '../types/GameLogArchive';
+import User from '../types/User';
 import Table from './Table';
 import { round } from 'lodash';
 
@@ -16,21 +17,21 @@ const GameLogArchivesTable = ({gameLogArchives}: GameLogArchivesTableParams) => 
         innerContent = <p>Error: game log archives could not be retrieved!</p>;
     } else {
         var rows = gameLogArchives.map((gameLogArchive: GameLogArchive) => {
-            let latestTablePlayers = gameLogArchive.maxGameLog.users.join(', ');
-            let latestTableDescription = `${gameLogArchive.maxGameLog.bgaTableId}, between ${latestTablePlayers}`;
+            let latestTablePlayers = gameLogArchive.maxGameLog.users.map((u: User) => { return u.name }).join(', ');
+            let latestTableDescription = <Link to={"https://boardgamearena.com/table?table=" + gameLogArchive.maxGameLog.bgaTableId}>{gameLogArchive.maxGameLog.bgaTableId}, between {latestTablePlayers}</Link>;
             return {
+                "Link": <Link to={gameLogArchive.url}>Download</Link>,
                 "Date": getDate(gameLogArchive.createdAt),
                 "Type": gameLogArchive.archiveType,
                 "Games": gameLogArchive.numGameLogs,
                 "Users": gameLogArchive.numUsers,
                 "Latest table": latestTableDescription,
                 "Size (MB)": round((gameLogArchive.sizeBytes) / (1024 * 1024), 0),
-                "Link": <Link to={gameLogArchive.url}>Download</Link>,
             }
         });
         innerContent = (
         <Table
-            cols={["Date", "Type", "Games", "Users", "Latest table", "Size (MB)", "Link"]}
+            cols={["Link", "Date", "Type", "Games", "Users", "Latest table", "Size, MB"]}
             rows={rows}
             key="game-log-archives"
             showFilters={false}
