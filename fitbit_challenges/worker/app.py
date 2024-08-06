86e0cf7143c685bb1ed339e777156f95e9afd52f
Charles OuGuo <shaldengeki@gmail.com>
2024-08-06 01:35:51 -0400

    Convert fitbit-challenges worker to use macro, and fixup a bunch of skeleton bugs (#360)
    - **Convert fitbit-challenges worker to use macro**
    - **Convert rebuild-and-run to Python**
    - **Add fly.toml for worker deployment**
    - **Deploy worker**
    - **Add fly.toml to skeleton worker**
    - **Add fly.toml for skeleton API and frontend, too**
    - **Fixup compose configuration for skeleton**

--- fitbit_challenges/worker/app.py
+++ fitbit_challenges/worker/app.py
@@ -1,11 +1,8 @@
 import datetime
 import time
-from base64 import b64encode
 from datetime import timezone
 from typing import Optional
-from urllib.parse import urlencode
 
-import requests
 from sqlalchemy import desc, update
 from sqlalchemy.sql.functions import now
 
