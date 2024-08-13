8f0c7977e40a1af58f2878c769ec86a21e754f48
Charles OuGuo <shaldengeki@gmail.com>
2024-08-13 00:05:39 -0400

    In worker, create cards & plays (#380)

--- ark_nova_stats/worker/app.py
+++ ark_nova_stats/worker/app.py
@@ -1,6 +1,6 @@
 import datetime
-import gzip
-import io
+import gc
+import json
 import logging
 import os
 import tarfile
@@ -11,8 +11,16 @@ from typing import Iterator, Optional
 import boto3
 from sqlalchemy import desc
 
+from ark_nova_stats.bga_log_parser.game_log import GameLog as BGAGameLog
 from ark_nova_stats.config import app, db
-from ark_nova_stats.models import GameLog, GameLogArchive, GameLogArchiveType, User
+from ark_nova_stats.models import (
+    Card,
+    CardPlay,
+    GameLog,
+    GameLogArchive,
+    GameLogArchiveType,
+    User,
+)
 
 max_delay = 12 * 60 * 60
 
@@ -118,6 +126,65 @@ def archive_logs_to_tigris(
     return new_archive
 
 
+def populate_card_play_actions() -> None:
+    logger.info(f"Populating card play actions.")
+    card_ids = set()
+
+    for game_log in GameLog.query.yield_per(10):  # type: ignore
+        parsed_log = BGAGameLog(**json.loads(game_log.log))
+        # First, create underlying card models.
+        for play in parsed_log.data.card_plays:
+            # Check to see if it exists.
+
+            if play.card.id in card_ids:
+                continue
+
+            find_card = Card.query.where(Card.bga_id == play.card.id).limit(1).all()
+            if not find_card:
+                card = Card(name=play.card.name, bga_id=play.card.id)  # type: ignore
+                logger.info(f"Staging card creation for: {play.card.id}")
+                db.session.add(card)
+                card_ids.add(play.card.id)
+
+    logger.info("Committing all the new cards.")
+    db.session.commit()
+
+    # Now create all the card plays.
+    logging.info("Creating card plays.")
+    for log_model in GameLog.query.yield_per(10):  # type: ignore
+        parsed_log = BGAGameLog(**json.loads(log_model.log))
+        for play in parsed_log.data.card_plays:
+            find_card = Card.query.where(Card.bga_id == play.card.id).limit(1).all()
+            card = find_card[0]
+
+            find_play = CardPlay.query.where(
+                CardPlay.game_log_id == log_model.id
+                and CardPlay.card_id == card.id
+                and CardPlay.user_id == play.player.id
+                and CardPlay.move == play.move
+            ).count()
+            if find_play > 0:
+                continue
+
+            logger.info(
+                f"Staging card play creation for game ID {log_model.id}, card {card.id}"
+            )
+
+            db.session.add(
+                CardPlay(  # type: ignore
+                    game_log_id=log_model.id,
+                    card=card,
+                    user_id=play.player.id,
+                    move=play.move,
+                )
+            )
+
+    logger.info("Committing all the new card plays.")
+    db.session.commit()
+
+    logger.info("Done creating new card plays!")
+
+
 API_SECRET_KEY = os.getenv("API_WORKER_SECRET")
 
 
@@ -128,6 +195,7 @@ def main() -> int:
             start = time.time()
             # Do work here.
             archive_logs_to_tigris(tigris_client)
+            populate_card_play_actions()
             delay = (start + max_delay) - time.time()
             if delay > 0:
                 time.sleep(delay)
