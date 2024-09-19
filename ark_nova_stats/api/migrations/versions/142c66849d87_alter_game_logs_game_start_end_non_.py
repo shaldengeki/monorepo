9fc00676870c5db205d9a59cc417aef241ee66a0
Charles Guo <shaldengeki@gmail.com>
2024-09-18 22:40:42 -0400

    In Ark game log table, make fields non-null

---
+++ ark_nova_stats/api/migrations/versions/142c66849d87_alter_game_logs_game_start_end_non_.py
@@ -0,0 +1,46 @@
+"""alter-game-logs-game-start-end-non-nullable
+
+Revision ID: 142c66849d87
+Revises: cfe96b7f8238
+Create Date: 2024-09-18 22:03:59.380204
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "142c66849d87"
+down_revision = "cfe96b7f8238"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(
+        "game_logs",
+        "game_start",
+        nullable=False,
+        existing_nullable=True,
+    )
+    op.alter_column(
+        "game_logs",
+        "game_end",
+        nullable=False,
+        existing_nullable=True,
+    )
+
+
+def downgrade():
+    op.alter_column(
+        "game_logs",
+        "game_start",
+        nullable=True,
+        existing_nullable=False,
+    )
+    op.alter_column(
+        "game_logs",
+        "game_end",
+        nullable=True,
+        existing_nullable=False,
+    )
