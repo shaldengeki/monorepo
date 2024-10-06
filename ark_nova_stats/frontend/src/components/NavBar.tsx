d80ee3f5a5d632341cc2cbe4b71d0a1876823e5c
Charles OuGuo <shaldengeki@gmail.com>
2024-10-06 17:52:19 -0400

    :technologist:

--- ark_nova_stats/frontend/src/components/NavBar.tsx
+++ ark_nova_stats/frontend/src/components/NavBar.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {PropsWithChildren} from 'react';
 import logo192 from './logo192.png';
 import { Link } from 'react-router-dom';
 
 type NavBarElementProps = {
-    text: string;
     link?: string;
+    text?: string;
     className?: string;
 }
 
-const NavBarElement = (props: NavBarElementProps) => {
+const NavBarElement = (props: PropsWithChildren<NavBarElementProps>) => {
     const defaultClassName = "dark:hover:text-slate-300 hover:text-slate-500";
     const actualClassName = props.className ? defaultClassName + " " + props.className : defaultClassName;
 
-    let innerContent = <p>{props.text}</p>
+    let innerContent = props.children;
+    if (props.text !== undefined) {
+        innerContent = <p>{props.text}</p>;
+    }
     if (props.link !== undefined) {
         innerContent = (<Link to={props.link}>
-            <p>{props.text}</p>
+            {innerContent}
         </Link>);
     }
 
@@ -35,8 +38,10 @@ const NavBar = (props: NavBarProps) => {
         <div className="sticky top-0 z-40 w-full backdrop-blur flex-none lg:border-b lg:border-slate-900/10 lg:dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
             <div className="max-w-screen-2xl mx-auto">
                 <div className="py-4 mx-4 lg:mx-0 lg:px-8 relative flex items-center gap-2 dark:text-slate-400">
-                    <img className="flex-none h-6 inline" src={logo192} alt="Fitbit app icon" />
-                    <NavBarElement link={'/'} text={"Ark Nova Games Database"} className={"font-bold"} />
+                    <NavBarElement link={'/'} className={"font-bold"}>
+                        <img className="flex-none h-6 inline" src={logo192} alt="Fitbit app icon" />
+                        <span>Ark Nova Games Database</span>
+                    </NavBarElement>
                     <NavBarElement link={'/emu_cup'} text={"Emu Cup"} />
                     <NavBarElement link={'/cards'} text={"Cards"} />
                 </div>
