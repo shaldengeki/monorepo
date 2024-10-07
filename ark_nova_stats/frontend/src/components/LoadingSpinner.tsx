fef58b9ef4952c1154162e695f1039f52e78ba21
Charles OuGuo <shaldengeki@gmail.com>
2024-10-06 21:13:31 -0400

    :bento: Factor all loading elements into a common component

---
+++ ark_nova_stats/frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,19 @@
+import React, {PropsWithChildren} from 'react';
+
+type LoadingSpinnerProps = {
+    className?: string;
+}
+
+const LoadingSpinner = (props: PropsWithChildren<LoadingSpinnerProps>) => {
+    const defaultClassNames = "";
+    const actualClassNames = props.className ? defaultClassNames + " " + props.className : defaultClassNames;
+
+    return (
+        <div className={actualClassNames}>
+            <p>Loading...</p>
+            {props.children}
+        </div>
+    );
+}
+
+export default LoadingSpinner;
--- ark_nova_stats/frontend/src/views/CardView.tsx
+++ ark_nova_stats/frontend/src/views/CardView.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { gql } from '@apollo/client/core';
 import { useQuery } from '@apollo/client/react/hooks';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-import PageContainer from '../components/PageContainer';
 import PageTitle from "../components/PageTitle";
 import CardInfoBox from '../components/CardInfoBox';
 import GameLogsTable from '../components/GameLogsTable';
 import Card from '../types/Card';
 import GameLog from '../types/GameLog';
 import UserPlayCount from '../types/UserPlayCount';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 export const USER_VIEW_QUERY = gql`
     query FetchCard($id: String!) {
@@ -63,7 +63,7 @@ const CardView = () => {
 
 
     let innerContent = <p></p>;
-    if (loading) innerContent = <p>Loading...</p>;
+    if (loading) innerContent = <LoadingSpinner />;
     else if (error) innerContent = <p>Error: {error.message}</p>;
     else {
         const card: Card = data.card;
