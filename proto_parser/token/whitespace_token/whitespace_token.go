e6ba7c4b3ce68d11659b050b9a1bbcbb77201244
Charles OuGuo <shaldengeki@gmail.com>
2025-02-06 01:30:52 -0500

    :loud_sound: Implement whitespace token parsing

--- proto_parser/token/whitespace_token/whitespace_token.go
+++ proto_parser/token/whitespace_token/whitespace_token.go
@@ -8,12 +8,19 @@ import (
 	pbtoken "github.com/shaldengeki/monorepo/proto_parser/proto/token"
 )
 
-func ParseWhitespaceToken(ctx context.Context, start int, body string) (pbtoken.Token, error) {
+func ParseWhitespaceToken(ctx context.Context, start int, body string) (pbtoken.WhitespaceToken, int, error) {
 	if start >= len(body) {
-		return pbtoken.Token{}, fmt.Errorf("Cannot parse position %i in body of length %i", start, len(body))
+		return pbtoken.WhitespaceToken{}, 0, fmt.Errorf("Cannot parse position %i in body of length %i", start, len(body))
 	}
 	if !strings.ContainsAny(string(body[start]), " \n\t") {
-		return pbtoken.Token{}, token.TokenNotParseableError(fmt.Sprintf("character %s is not whitespace", string(body[start])))
+		return pbtoken.WhitespaceToken{}, 0, token.TokenNotParseableError(fmt.Sprintf("character %s is not whitespace", string(body[start])))
 	}
-	return pbtoken.Token{}, nil
+
+	stringToScan := string(body[start:])
+	removedBody := strings.TrimLeft(stringToScan, " \n\t")
+	whitespaceLength := len(stringToScan) - len(removedBody)
+
+	return pbtoken.WhitespaceToken{
+		Spaces: string(body[start:start+whitespaceLength]),
+	}, start + whitespaceLength, nil
 }
--- proto_parser/token/whitespace_token/whitespace_token_test.go
+++ proto_parser/token/whitespace_token/whitespace_token_test.go
@@ -10,13 +10,13 @@ import (
 
 func TestParseWhitespaceToken_WithEmptyString_ReturnsError(t *testing.T) {
 	ctx := context.Background()
-	_, err := ParseWhitespaceToken(ctx, 0, "")
+	_, _, err := ParseWhitespaceToken(ctx, 0, "")
 	assert.NotNil(t, err)
 }
 
 func TestParseWhitespaceToken_WithNonWhitespace_ReturnsError(t *testing.T) {
 	ctx := context.Background()
-	_, err := ParseWhitespaceToken(ctx, 0, "syntax  f")
+	_, _, err := ParseWhitespaceToken(ctx, 0, "syntax  f")
 	require.NotNil(t, err)
 	errType := token.TokenNotParseableError("test")
 	assert.ErrorAs(t, err, &errType)
@@ -25,8 +25,17 @@ func TestParseWhitespaceToken_WithNonWhitespace_ReturnsError(t *testing.T) {
 func TestParseWhitespaceToken_WithWhitespaceOnly_ReturnsWholeBody(t *testing.T) {
 	ctx := context.Background()
 	body := "   \n\t\n   \t     "
-	res, err := ParseWhitespaceToken(ctx, 0, body)
+	res, idx, err := ParseWhitespaceToken(ctx, 0, body)
 	require.Nil(t, err)
-	require.NotNil(t, res.GetWhitespaceToken())
-	assert.Equal(t, body, res.GetWhitespaceToken().Spaces)
+	assert.Equal(t, body, res.Spaces)
+	assert.Equal(t, len(body), idx)
+}
+
+func TestParseWhitespaceToken_WithPartialWhitespace_ReturnsLeadingWhitespace(t *testing.T) {
+	ctx := context.Background()
+	body := "   \n  \t foo bar"
+	res, idx, err := ParseWhitespaceToken(ctx, 0, body)
+	require.Nil(t, err)
+	assert.Equal(t, "   \n  \t ", res.Spaces)
+	assert.Equal(t, 8, idx)
 }
