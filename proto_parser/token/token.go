28c8c8fbfa4911ba4050c5ae23ccfe9e7f53699d
Charles OuGuo <shaldengeki@gmail.com>
2025-02-06 00:51:03 -0500

    :closed_lock_with_key: Add basic test for whitespace token

--- proto_parser/token/token.go
+++ proto_parser/token/token.go
@@ -2,8 +2,15 @@ package token
 
 import (
 	"context"
+	"fmt"
 	pbtoken "github.com/shaldengeki/monorepo/proto_parser/proto/token"
 )
+
+type TokenNotParseableError string
+func (e TokenNotParseableError) Error() string {
+	return fmt.Sprintf("Cannot parse body as token: %v", string(e))
+}
+
 
 func ParseTokens(ctx context.Context, body string) ([]pbtoken.Token, error) {
 	return []pbtoken.Token{}, nil
