49cdcfdd4ad13188d322100ab51085d73acf1388
Charles OuGuo <shaldengeki@gmail.com>
2025-02-09 20:15:15 -0500

    :lipstick: In proto-parser, parse stdin

--- proto_parser/main/main.go
+++ proto_parser/main/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io"
+	"log"
+	"os"
 	"github.com/shaldengeki/monorepo/proto_parser/token"
 )
 
 func main() {
 	ctx := context.Background()
-	fmt.Println("hello!")
-	token.ParseTokens(ctx, "")
+	logger := log.Default()
+
+	protoText, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		logger.Fatalf("Could not read stdin: %v", err)
+	}
+
+	tokens, err := token.ParseTokens(ctx, string(protoText))
+	if err != nil {
+		logger.Fatalf("Could not parse tokens: %v", err)
+	}
+	logger.Printf("Tokens: %v", tokens)
+
 	return
 }
