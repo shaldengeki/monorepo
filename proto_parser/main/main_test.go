f30709cfe171929026b9675e27dc1206f2808062
Charles OuGuo <shaldengeki@gmail.com>
2025-02-11 00:19:35 -0500

    :beers: Set up syntax tests

--- proto_parser/main/main_test.go
+++ proto_parser/main/main_test.go
@@ -13,3 +13,35 @@ func TestEmpty(t *testing.T) {
 	require.Nil(t, err)
 	assert.Equal(t, 0, len(res.Nodes))
 }
+
+func TestSyntax_WithProto3_WithSingleQuotes_ReturnsNode(t *testing.T) {
+	ctx := context.Background()
+	res, err := ParseProtoText(ctx, "syntax = 'proto3';")
+	require.Nil(t, err)
+	assert.Equal(t, 1, len(res.Nodes))
+	assert.Equal(t, "proto3", res.Nodes[0].GetSyntaxNode().Syntax)
+}
+
+func TestSyntax_WithProto3_WithDoubleQuotes_ReturnsNode(t *testing.T) {
+	ctx := context.Background()
+	res, err := ParseProtoText(ctx, "syntax = \"proto3\";")
+	require.Nil(t, err)
+	assert.Equal(t, 1, len(res.Nodes))
+	assert.Equal(t, "proto3", res.Nodes[0].GetSyntaxNode().Syntax)
+}
+
+func TestSyntax_WithProto2_WithSingleQuotes_ReturnsNode(t *testing.T) {
+	ctx := context.Background()
+	res, err := ParseProtoText(ctx, "syntax = 'proto2';")
+	require.Nil(t, err)
+	assert.Equal(t, 1, len(res.Nodes))
+	assert.Equal(t, "proto2", res.Nodes[0].GetSyntaxNode().Syntax)
+}
+
+func TestSyntax_WithProto2_WithDoubleQuotes_ReturnsNode(t *testing.T) {
+	ctx := context.Background()
+	res, err := ParseProtoText(ctx, "syntax = \"proto2\";")
+	require.Nil(t, err)
+	assert.Equal(t, 1, len(res.Nodes))
+	assert.Equal(t, "proto2", res.Nodes[0].GetSyntaxNode().Syntax)
+}
