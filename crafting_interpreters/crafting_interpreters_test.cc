cbba149074eb4406d25f84fc1009190e599fea8a
Charles Guo <shaldengeki@gmail.com>
2025-03-18 21:48:44 -0400

    Add rest of the tests

--- crafting_interpreters/crafting_interpreters_test.cc
+++ crafting_interpreters/crafting_interpreters_test.cc
@@ -3,8 +3,53 @@
 
 #include "crafting_interpreters.h"
 
-TEST(Static, ProperlyLinked) {
+TEST(CraftingInterpretersTest, EmptyList) {
     DoublyLinkedList* x = new DoublyLinkedList();
-    std::string* val = new std::string("hello");
-    EXPECT_EQ(x->insert(val), nullptr);
+    EXPECT_EQ(x->head, nullptr);
+
+    std::string* v = new std::string("not found");
+    EXPECT_EQ(x->find(v), nullptr);
+    EXPECT_EQ(x->remove(v), nullptr);
+}
+
+TEST(CraftingInterpretersTest, InsertsNewValue) {
+    DoublyLinkedList* x = new DoublyLinkedList();
+    std::string* v = new std::string("new value");
+
+    DoublyLinkedListNode* n = x->insert(v);
+    EXPECT_NE(n, nullptr);
+    EXPECT_EQ(x->head, n);
+    EXPECT_EQ(n->value, v);
+
+    DoublyLinkedListNode* n2 = x->insert(v);
+    EXPECT_NE(n2, nullptr);
+    EXPECT_NE(n2, n);
+}
+
+TEST(CraftingInterpretersTest, FindsExistingValue) {
+    DoublyLinkedList* x = new DoublyLinkedList();
+    std::string* v = new std::string("new value");
+    std::string* v2 = new std::string("alt value");
+    std::string* v3 = new std::string("not found value");
+
+    DoublyLinkedListNode* n = x->insert(v);
+    DoublyLinkedListNode* n2 = x->insert(v2);
+
+    ASSERT_EQ(x->find(v), n);
+    ASSERT_EQ(x->find(v2), n2);
+    ASSERT_EQ(x->find(v3), nullptr);
+}
+
+TEST(CraftingInterpretersTest, RemovesExistingValues) {
+    DoublyLinkedList* x = new DoublyLinkedList();
+    std::string* v = new std::string("new value");
+    std::string* v2 = new std::string("alt value");
+    std::string* v3 = new std::string("not found value");
+
+    DoublyLinkedListNode* n = x->insert(v);
+    DoublyLinkedListNode* n2 = x->insert(v2);
+
+    ASSERT_EQ(x->remove(v), n);
+    ASSERT_EQ(x->remove(v2), n2);
+    ASSERT_EQ(x->remove(v3), nullptr);
 }
