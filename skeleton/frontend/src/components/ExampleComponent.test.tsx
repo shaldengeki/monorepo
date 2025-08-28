fd75836d191691ef86b5aab3d677b0c532b31279
Charles Guo <shaldengeki@gmail.com>
2025-08-27 23:08:45 -0400

    :card_file_box: Explicitly specify rxjs

--- skeleton/frontend/src/components/ExampleComponent.test.tsx
+++ skeleton/frontend/src/components/ExampleComponent.test.tsx
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom";
 import { render, screen } from '@testing-library/react';
 import ExampleComponent from './ExampleComponent';
 import { MockedProvider } from "@apollo/client/testing/react";
