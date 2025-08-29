9788f001fef881910f430d204741f77e61a63054
Charles Guo <shaldengeki@gmail.com>
2025-08-29 02:22:05 -0400

    :poop: [wip] get some more components passing

--- ark_nova_stats/frontend/src/components/PageLink.test.tsx
+++ ark_nova_stats/frontend/src/components/PageLink.test.tsx
@@ -2,17 +2,21 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react';
 import PageLink from './PageLink';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 
 it('should render contents when provided text', async () => {
     render(
-        <PageLink to={"foo"}>This is my link text!</PageLink>
+        <PageLink to={"foo"}>This is my link text!</PageLink>,
+        {wrapper: BrowserRouter}
     );
     expect(await screen.findByText("This is my link text!")).toBeInTheDocument();
 });
 
 it('should render contents when provided an element', async () => {
     render(
-        <PageLink to={"foo"}><div>This is my link text within an element!</div></PageLink>
+        <PageLink to={"foo"}><div>This is my link text within an element!</div></PageLink>,
+        {wrapper: BrowserRouter}
     );
     expect(await screen.findByText("This is my link text within an element!")).toBeInTheDocument();
 });
