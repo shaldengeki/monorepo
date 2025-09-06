f69661e0d555728afd9c18d6ea3edd285f296c42
Charles Guo <shaldengeki@gmail.com>
2025-09-06 16:16:56 -0400

    :globe_with_meridians: Fixup navbar

--- fitbit_challenges/frontend/src/App.tsx
+++ fitbit_challenges/frontend/src/App.tsx
@@ -5,18 +5,40 @@ import {
   Navigate
 } from "react-router-dom";
 
+import { useQuery } from "@apollo/client/react/hooks";
 import ChallengesListingView from './views/ChallengesListingView';
 import ChallengeView from './views/ChallengeView';
 import AuthView from './views/AuthView';
 import logo192 from './logo192.png';
+import fitbit from './fitbit.png';
 import NavBar, {NavBarElement} from '../../../react_library/NavBar';
 import PageContainer from '../../../react_library/PageContainer';
+import { FETCH_CURRENT_USER_QUERY } from "./queries";
 
 function App() {
+    const { loading, error, data } = useQuery(
+        FETCH_CURRENT_USER_QUERY,
+    );
+
   const navbar = (
-    <NavBar logo={logo192} title="Ark Nova Games Database">
-      <NavBarElement link={'/emu_cup'} text={"Emu Cup"} />
-      <NavBarElement link={'/cards'} text={"Cards"} />
+    <NavBar logo={logo192}>
+      <NavBarElement link={'/challenges'} text={"Challenges"} />
+      {
+          loading && <p>Loading...</p>
+      }
+      {
+          error && <p>Error loading login state</p>
+      }
+      {
+          data && data.currentUser === null &&
+          <NavBarElement link={'/auth'}>
+              <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
+              <span className="font-bold">Sign in with Fitbit</span>
+          </NavBarElement>
+      }
+      {
+          data && data.currentUser && <p className="ml-auto">{data.currentUser.displayName}</p>
+      }
     </NavBar>
   )
 
