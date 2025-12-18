22031de0f8bf904cae96bf078317ffd3f20bff10
Charles OuGuo <shaldengeki@gmail.com>
2025-12-18 01:58:39 -0500

    Add tests for turn

--- games/tictactoe/server/server_test.go
+++ games/tictactoe/server/server_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"testing"
+	pb "github.com/shaldengeki/monorepo/games/tictactoe/proto"
 	pbserver "github.com/shaldengeki/monorepo/games/tictactoe/proto/server"
 	"github.com/shaldengeki/monorepo/games/tictactoe/game_state_provider"
 	"github.com/stretchr/testify/assert"
@@ -20,3 +21,33 @@ func TestValidateState_WithEmptyState_ReturnsValid(t *testing.T) {
 	require.NoError(t, err)
 	assert.Empty(t, res.ValidationErrors)
 }
+
+// int32 turn = 1;
+func TestValidateState_Turn(t *testing.T) {
+	ctx := context.Background()
+
+	emptyProvider := game_state_provider.NewEmptyGameStateProvider()
+	server := New(emptyProvider)
+
+	// Turn = 1
+	request := pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 1, Board: &pb.Board{Rows: 1, Columns: 1}}}
+	res, err := server.ValidateState(ctx, &request)
+	require.NoError(t, err)
+	assert.Empty(t, res.ValidationErrors)
+
+
+	// Turn = <= 0
+	request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 0, Round: 1, Board: &pb.Board{Rows: 1, Columns: 1}}}
+	res, err = server.ValidateState(ctx, &request)
+	require.NoError(t, err)
+	assert.NotEmpty(t, res.ValidationErrors)
+	request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: -1, Round: 1, Board: &pb.Board{Rows: 1, Columns: 1}}}
+	res, err = server.ValidateState(ctx, &request)
+	require.NoError(t, err)
+	assert.NotEmpty(t, res.ValidationErrors)
+}
+
+// int32 round = 2;
+// bool finished = 3;
+// repeated Score scores = 4;
+// Board board = 5;
