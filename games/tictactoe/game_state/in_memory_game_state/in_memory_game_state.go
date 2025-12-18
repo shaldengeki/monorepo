1d66df1531ca74d05bb61e95799af4620a7a25de
Charles OuGuo <shaldengeki@gmail.com>
2025-12-18 02:26:10 -0500

    Add InMemoryGameState provider

---
+++ games/tictactoe/game_state/in_memory_game_state/in_memory_game_state.go
@@ -0,0 +1,28 @@
+package in_memory_game_state
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/shaldengeki/monorepo/games/tictactoe/proto"
+	"github.com/shaldengeki/monorepo/games/tictactoe/game_state"
+)
+
+type InMemoryGameState struct {
+	game_state.GameState
+
+    gameStates map[string]*proto.GameState
+}
+
+func (s InMemoryGameState) GetState(ctx context.Context, gameId string) (*proto.GameState, error) {
+	state, found := s.gameStates[gameId]
+	if !found {
+		return nil, fmt.Errorf("could not find game state for id %s", gameId)
+	}
+
+	return state, nil
+}
+
+func NewInMemoryGameState(initialState map[string]*proto.GameState) game_state.GameState {
+	return InMemoryGameState{gameStates: initialState}
+}
