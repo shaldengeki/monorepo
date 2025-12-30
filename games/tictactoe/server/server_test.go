8e376c5ee0ffeee204a3d30c9d8369dc9573ec27
Charles OuGuo <shaldengeki@gmail.com>
2025-12-30 10:28:01 +0900

    :stethoscope: Restructure test a bit

--- games/tictactoe/server/server_test.go
+++ games/tictactoe/server/server_test.go
@@ -10,69 +10,58 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestValidateState_WithEmptyState_ReturnsValid(t *testing.T) {
+func TestValidateState(t *testing.T) {
 	ctx := context.Background()
-
-	emptyProvider := empty_game_state.NewEmptyGameState()
-	server := New(emptyProvider)
-	request := pbserver.ValidateStateRequest{}
-
-	res, err := server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.Empty(t, res.ValidationErrors)
-}
-
-func TestValidateState_Turn(t *testing.T) {
-	ctx := context.Background()
-
 	emptyProvider := empty_game_state.NewEmptyGameState()
 	server := New(emptyProvider)
 
-	// Turn = 1
-	request := pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 1}}
-	res, err := server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.Empty(t, res.ValidationErrors)
+	t.Run("ValidWithEmptyState", func(t *testing.T) {
+		request := pbserver.ValidateStateRequest{}
 
+		res, err := server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.Empty(t, res.ValidationErrors)
+	})
 
-	// Turn = <= 0
-	request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 0, Round: 1}}
-	res, err = server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.NotEmpty(t, res.ValidationErrors)
-	request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: -1, Round: 1}}
-	res, err = server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.NotEmpty(t, res.ValidationErrors)
+	t.Run("Turn", func(t *testing.T) {
+		// Turn = 1
+		request := pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 1}}
+		res, err := server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.Empty(t, res.ValidationErrors)
+
+		// Turn = <= 0
+		request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 0, Round: 1}}
+		res, err = server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.NotEmpty(t, res.ValidationErrors)
+		request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: -1, Round: 1}}
+		res, err = server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.NotEmpty(t, res.ValidationErrors)
+	})
+
+	t.Run("Round", func(t *testing.T) {
+		// Round = 1
+		request := pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 1}}
+		res, err := server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.Empty(t, res.ValidationErrors)
+
+		// Round = <= 0
+		request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 0}}
+		res, err = server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.NotEmpty(t, res.ValidationErrors)
+		request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: -1}}
+		res, err = server.ValidateState(ctx, &request)
+		require.NoError(t, err)
+		assert.NotEmpty(t, res.ValidationErrors)
+	})
+	// bool finished = 3;
+	// repeated Score scores = 4;
+	// Board board = 5;
 }
-
-// int32 round = 2;
-func TestValidateState_Round(t *testing.T) {
-	ctx := context.Background()
-
-	emptyProvider := empty_game_state.NewEmptyGameState()
-	server := New(emptyProvider)
-
-	// Round = 1
-	request := pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 1}}
-	res, err := server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.Empty(t, res.ValidationErrors)
-
-
-	// Round = <= 0
-	request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: 0}}
-	res, err = server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.NotEmpty(t, res.ValidationErrors)
-	request = pbserver.ValidateStateRequest{GameState: &pb.GameState{Turn: 1, Round: -1}}
-	res, err = server.ValidateState(ctx, &request)
-	require.NoError(t, err)
-	assert.NotEmpty(t, res.ValidationErrors)
-}
-// bool finished = 3;
-// repeated Score scores = 4;
-// Board board = 5;
 
 func TestMakeMove(t *testing.T) {
 	assert.NotNil(t, nil)
